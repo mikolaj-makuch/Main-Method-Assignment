81ed78f3c2d830291fdaa2562fc144360aaee650
micuch11 <micuch1126@gmail.com>
2024-03-04 08:31:30 +0000

    Main Method Assignment

---
+++ Main Method Assignment/Main Method Assignment/MathMethods2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Main_Method_Assignment
+{
+    public class MathMethods2
+    {
+        //First method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 7;
+        }
+        //Second method that takes a decimal
+        public int MathOp(decimal number)
+        {
+            //Convert the decimal to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum * 20;
+        }
+        //Third MathOp method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum % 3;
+        }
+    }
+}
